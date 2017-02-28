9b006d5f09c44e872dca3854699942cc267a23e4
blueblance <blueblance@DESKTOP-12CASA7>
2017-03-01 01:10:09 +0800

    練習

--- WindowsFormsApplication4/Form1.Designer.cs
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApplication4
+﻿namespace delegate_practice
 {
     partial class Form1
     {
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(376, 132);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(522, 132);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(394, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(400, 259);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(394, 334);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(505, 334);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(875, 540);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- WindowsFormsApplication4/Form1.cs
+++ WindowsFormsApplication4/Form1.cs
@@ -7,14 +7,172 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
-namespace WindowsFormsApplication4
+namespace delegate_practice
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public delegate string testhandler1(string a);
+        public delegate void tbstringhandler(string a, Settextboxcallback sb, TextBox tb);
+        public delegate void testhand();
+        public delegate void loophander(TextBox tb, int i);
+        public delegate void loophandler2();
+        public delegate void mipihander(mipi mipi);
+        BackgroundWorker bb = null;
+        Thread workThread = null;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Thread> threadlist = new List<Thread>();
+            mipi m1 = new mipi(500, textBox1);
+            mipi m2 = new mipi(50, textBox2);
+            //testhand tt = test1;
+            //tt += test2;
+            //tt();
+            //Settextboxcallback sb = Settextbox;
+            //sb(textBox1, "hello 1");
+            //Thread t1 = new Thread(delegate () { mipiloop(m1); });
+            //threadlist.Add(t1);
+            //Thread t2 = new Thread(delegate () { mipiloop2(m2); });
+            //threadlist.Add(t2);
+            //foreach(Thread tt in threadlist )
+            //{
+            //    tt.Start();
+            //}
+            //mipihander tt = mipiloop2;
+            //tt += mipiloop;
+            //tt(m1);
+            //Thread t1 = new Thread(delegate () { mipiloop(m1); });
+            ThreadStart tstar = new ThreadStart(nofunc);
+            tstar += nofunc;
+            Thread t2 = new Thread(tstar);
+
+
+            //t1.Start();
+            //t2.Start();
+            //t1.Join();
+            //loophander lp = loop_test1;
+            //lp += loop_test2;
+
+            //lp(textBox1, 50);
+
+        }
+
+        public void nofunc()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Settextbox(textBox1, i.ToString());
+                Thread.Sleep(100);
+            }
+
+        }
+        public void mipiloop(mipi mipi)
+        {
+            for (int i = 0; i < mipi.bitrate; i++)
+            {
+                Settextbox(mipi.tb, i.ToString());
+                Thread.Sleep(100);
+            }
+        }
+        public void mipiloop2(mipi mipi)
+        {
+            for (int i = 0; i < mipi.bitrate; i++)
+            {
+                Settextbox(mipi.tb, (i * 2).ToString());
+                Thread.Sleep(100);
+            }
+        }
+
+        public void loop_test1(TextBox tb, int i)
+        {
+            for (int a = 0; a < i; a++)
+            {
+                Settextbox(tb, i.ToString());
+                Thread.Sleep(100);
+            }
+        }
+
+        public void loop_test2(TextBox tb, int i)
+        {
+            for (int a = 0; a < i; a++)
+                Settextbox(tb, (i * 2).ToString());
+        }
+
+        public void test1()
+        {
+            Settextbox(textBox1, "test1");
+        }
+
+        public void test2()
+        {
+            Settextbox(textBox2, "test2");
+        }
+
+        public delegate void Settextboxcallback(TextBox tb, String data);
+        public void Settextbox(TextBox tb, String data)
+        {
+            if (tb.InvokeRequired)
+            {
+                Settextboxcallback Sb = new Settextboxcallback(Settextbox);
+                tb.BeginInvoke(Sb, new object[] { tb, data });
+            }
+            else
+            {
+                tb.Text = data;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            workThread.Abort();
+            workThread = null;
+
+            bb.Dispose();
+            bb = null;
+
+        }
+
+        void stop_thread(object sender, EventArgs e)
+        {
+            if (e is Form1)
+            {
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (bb == null && workThread == null)
+            {
+                mipihander mm = mipiloop;
+                mipi m1 = new mipi(500, textBox1);
+                bb = new BackgroundWorker();
+                bb.DoWork += (delegate (object workObject, DoWorkEventArgs workEvent) {
+                    workThread = Thread.CurrentThread;
+                    mipiloop(m1);
+
+                });
+                bb.RunWorkerAsync();
+            }
+        }
+    }
+
+    public class mipi
+    {
+        public int bitrate;
+        public TextBox tb;
+
+        public mipi(int a, TextBox tb)
+        {
+            bitrate = a;
+            this.tb = tb;
         }
     }
 }
